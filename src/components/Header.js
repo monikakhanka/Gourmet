43298166d5c9bf90ab007f728f2686036d83d2c4
monika <monikakhanka@gmail.com>
2024-11-21 17:41:14 +0530

    ui fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 
 
 export const Header = () => {
-    const [btnName, setBtnName] = useState("Login");
     const onlineStatus = useOnlineStatus();
 
     const {loggedInUser} = useContext(UserContext);
@@ -16,24 +15,19 @@ export const Header = () => {
     const cartItems = useSelector((store) => store.cart.items);
    
     return (
-        <div className="flex justify-between bg-red-100 shadow-xl m-2">
+        <div className="flex justify-between font-serif bg-red-100 shadow-xl m-2">
             <div className="w-40 h-30">
                 <Link to="/"><img src={LOGO_URL} alt="logo" /></Link>
             </div>  
             <div className="flex items-center">
                 <ul className="flex px-10">
-                    <li className="px-4 py-6">Online status: {onlineStatus ? <>&#x2713;</>: <>&times;</>}</li>
-                    <li className="px-4 py-6"><Link to="/">Home</Link></li>
-                    <li className="px-4 py-6"><Link to="/about">About Us</Link></li>
-                    <li className="px-4 py-6"><Link to="/contact">Contact Us</Link></li>
-                    <li className="px-4 py-6"><Link to="/grocery">Grocery</Link></li>
-                    <li className="px-4 py-6 font-bold text-xl"><Link to="/cart">Cart-({cartItems.length} Items)</Link></li>
-                    
-                    <button className="px-4 py-6" onClick={() => {
-                        btnName === "Login" ? setBtnName("logout"): setBtnName("Login");
-                        }
-                    }>{btnName}</button>
-                    <li className="px-4 py-6 font-bold">{loggedInUser}</li>
+                    <li className="px-4 py-6 font-normal text-xl">Online status: {onlineStatus ? <>&#x2713;</>: <>&times;</>}</li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl"><Link to="/">Home</Link></li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl"><Link to="/about">About Us</Link></li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl"><Link to="/contact">Contact Us</Link></li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl"><Link to="/grocery">Grocery</Link></li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl"><Link to="/cart">Cart-({cartItems.length} Items)</Link></li>
+                    <li className="px-4 py-6 font-normal hover:font-bold text-xl">{loggedInUser}</li>
                 </ul>
             </div>
         </div>
