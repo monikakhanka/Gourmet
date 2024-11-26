c5c2f06b28db549b345105dad7299ec98515d231
monika <monikakhanka@gmail.com>
2024-11-26 16:28:12 +0530

    changes for prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,12 +3,12 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 export const LOGO_URL ="https://thumbs.dreamstime.com/b/fast-food-logo-cartoon-design-advertising-poster-brochure-banner-graphic-illustration-image-generative-ai-fast-food-327213943.jpg";
 
 // for Production
-// export const MENU_API = "/api/menu/";
+export const MENU_API = "/api/menu/";
 // for local
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=";
+// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=";
 
 // for Production
-// export const RESTAURANTS_API = "/api/restaurants";
+export const RESTAURANTS_API = "/api/restaurants";
 
 // for local
-   export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5946784&lng=73.7095365&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+//   export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5946784&lng=73.7095365&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
