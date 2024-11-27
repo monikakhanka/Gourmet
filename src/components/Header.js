202eafebf2c5e7b82ea94677b35c3bfe0317150f
monika <monikakhanka@gmail.com>
2024-11-27 16:18:56 +0530

    header fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,9 @@
-import { useContext, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
-import useOnlineStatus from "../utils/useOnlineStatus";
-import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
 
 
 export const Header = () => {
-    const onlineStatus = useOnlineStatus();
-
-    const {loggedInUser} = useContext(UserContext);
 
     // subscribing to the store using Selector
     const cartItems = useSelector((store) => store.cart.items);
@@ -21,13 +15,12 @@ export const Header = () => {
             </div>  
             <div className="flex justify-evenly items-center font-serif text-lg">
                 <ul className="flex px-10">
-                    <li className="px-4 py-6 font-normal ">Online status: {onlineStatus ? <>&#x2713;</>: <>&times;</>}</li>
                     <li className="px-4 py-6 font-normal hover:text-orange-500"><Link to="/">Home</Link></li>
                     <li className="px-4 py-6 font-normal hover:text-orange-500"><Link to="/about">About Us</Link></li>
                     <li className="px-4 py-6 font-normal hover:text-orange-500"><Link to="/contact">Contact Us</Link></li>
                     <li className="px-4 py-6 font-normal hover:text-orange-500"><Link to="/grocery">Grocery</Link></li>
                     <li className="px-4 py-6 font-normal hover:text-orange-500"><Link to="/cart">Cart-({cartItems.length} Items)</Link></li>
-                    <li className="px-4 py-6 font-norma  hover:text-orange-500">{loggedInUser}</li>
+                    
                 </ul>
             </div>
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,10 @@ export const LOGO_URL ="https://thumbs.dreamstime.com/b/fast-food-logo-cartoon-d
 // for Production
 export const MENU_API = "/api/menu/";
 // for local
-// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=";
+//export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=";
 
 // for Production
 export const RESTAURANTS_API = "/api/restaurants";
 
 // for local
-//   export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5946784&lng=73.7095365&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+//  export const RESTAURANTS_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5946784&lng=73.7095365&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
